116299d116266469e988c8df27f580acbb9a3678
DeivyssonLucas <125076967+DeivyssonLucas@users.noreply.github.com>
2023-03-23 23:21:36 -0300

    Delete aula11 directory

--- aula11/ex008.js
+++
@@ -1,7 +0,0 @@
-var vel = 54;
-console.log(`A velocidade da moto é ${vel}km/h`);
-if (vel > 60) {
-  //condição simples
-  console.log(`Você ultrapassou a velocidade permitida. MULTADO!`);
-}
-console.log(`Dirija sempre usando cinto de segurança!`);
